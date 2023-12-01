bbbd0de1b47c609be2cf62445d57e81bdb95a125
andresmarpz <andresmarpz@gmail.com>
2023-12-01 16:40:12 -0300

    improve algorithm speed by looping through lines once

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -23,24 +23,23 @@ fn main() {
 
     // Loop through the lines
     for line in lines {
-        let first_number_in_string = line
-            .find(|c: char| c.is_numeric())
-            .map(|index| line.chars().nth(index).unwrap())
-            .unwrap()
-            .to_string()
-            .chars()
-            .next()
-            .unwrap();
-        let last_number_in_string = line
-            .rfind(|c: char| c.is_numeric())
-            .map(|index| line.chars().nth(index).unwrap())
-            .unwrap()
-            .to_string()
-            .chars()
-            .next()
-            .unwrap();
+        let chars = line.chars();
+        let mut first_number_in_string: i32 = -1;
+        let mut last_number_in_string = -1;
 
-        // Convert the two numbers to integers as we need it to be {}{}
+        // Loop through the characters in the line
+        for char in chars {
+            // Only care about numeric characters
+            if char.is_numeric() {
+                if first_number_in_string == -1 {
+                    first_number_in_string = char.to_digit(10).unwrap() as i32;
+                }
+
+                last_number_in_string = char.to_digit(10).unwrap() as i32;
+            }
+        }
+
+        // Group the first and last numbers together and parse them as a u32
         let pair = format!("{}{}", first_number_in_string, last_number_in_string);
         let result = pair.parse::<u32>().unwrap();
 
