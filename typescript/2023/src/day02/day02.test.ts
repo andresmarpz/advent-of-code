c0b71d7fd9607ab28ce9622cee3d818e688aa829
andresmarpz <andresmarpz@gmail.com>
2023-12-12 19:00:30 -0300

    solve day 2

---
+++ typescript/2023/src/day02/day02.test.ts
@@ -0,0 +1,26 @@
+import { part1, part2 } from "2023/src/day02/day02";
+import { expect, test } from "bun:test";
+
+const exampleData = `
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`;
+
+const exampleData2 = `
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`;
+
+test("day 2 / part 1", () => {
+  expect(part1(exampleData)).toBe(8);
+});
+
+test("day 2 / part 2", () => {
+  expect(part2(exampleData2)).toBe(2286);
+});
